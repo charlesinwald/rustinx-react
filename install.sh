90f94a8e76db6f851ef5a3c69e648b49c2e579f8
Charles Inwald <cinwald@gmail.com>
2024-03-18 19:17:19 -0400

    Update service configuration

--- install.sh
+++ install.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-SERVICE_URL="https://github.com/charlesinwald/rustinx-react/raw/main/rustinx.service"
+# Define the URL of the service file
+SERVICE_URL="https://raw.githubusercontent.com/charlesinwald/rustinx-react/main/rustinx.service"
+
+# Define where to save the downloaded service file (temporary location)
 TEMP_SERVICE_PATH="/tmp/rustinx.service"
 
-echo "Downloading the service file from $SERVICE_URL..."
+# Download the service file
 curl -o "$TEMP_SERVICE_PATH" "$SERVICE_URL"
 
+# Get the current username
 USERNAME=$(whoami)
-HOME_DIR=$(eval echo ~$USERNAME)
-SERVICE_PATH="/etc/systemd/system/rustinx.service"
 
-echo "Configuring the service file for user $USERNAME..."
-sed "s|/username/|$HOME_DIR/|g" "$TEMP_SERVICE_PATH" | sed "s/myuser/$USERNAME/g" > "$SERVICE_PATH"
+sed -i "s|/username/|/$USERNAME/|g" "$TEMP_SERVICE_PATH"
 
-# Clean up the temporary file
-rm "$TEMP_SERVICE_PATH"
+# Now move the modified service file to the systemd directory
+sudo mv "$TEMP_SERVICE_PATH" "/etc/systemd/system/rustinx.service"
 
-echo "Reloading systemd..."
+# Reload systemd, enable and start the service
 sudo systemctl daemon-reload
-
-# Unmask the service before enabling and starting
-echo "Unmasking the service..."
-sudo systemctl unmask rustinx.service
-
-echo "Enabling and starting the rustinx service..."
 sudo systemctl enable rustinx.service
 sudo systemctl start rustinx.service
-
-echo "Rustinx service installation and startup process complete."
